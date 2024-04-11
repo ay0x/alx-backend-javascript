eb301ca9174ef454c1a88456b52220216be166b4
ay0x <85720179+ay0x@users.noreply.github.com>
2024-04-11 18:42:14 +0200

    Fixed attr type validation before instance initialization

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,13 +1,20 @@
 export default class HolbertonCourse {
   constructor(name, length, students) {
+    if (typeof name !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+
+    if (typeof length !== 'number') {
+      throw new TypeError('Length must be a number');
+    }
+
+    if (!Array.isArray(students)) {
+      throw new TypeError('Students must be an array');
+    }
+
     this._name = name;
     this._length = length;
-    this._students = Array.isArray(students) ? students : [];
-
-    // Verify data types
-    if (typeof this._name !== 'string' || typeof this._length !== 'number' || !Array.isArray(this._students)) {
-      throw new TypeError('Invalid attribute type');
-    }
+    this._students = students;
   }
 
   // Getter and Setter for name
