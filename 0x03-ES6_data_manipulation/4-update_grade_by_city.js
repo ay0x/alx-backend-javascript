581f8e8679f19c8d6447c552662c26aa7d6746c6
ay0x <85720179+ay0x@users.noreply.github.com>
2024-04-16 20:37:15 +0200

    Update 4-update_grade_by_city.js

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,16 +1,12 @@
-export default function updateStudentGradeByCity(students, city, newGrades) {
-  const studentsInCity = students.filter((student) => student.location === city);
-  return studentsInCity.map((student) => {
-    const filteredGrades = newGrades.filter((grade) => grade.studentId === student.id);
-    if (filteredGrades.length > 0) {
-      return {
-        ...student,
-        grade: filteredGrades[0].grade,
-      };
-    }
+export default function updateStudentGradeByCity(studentList, city, newGrades) {
+  const studentsInCity = studentList.filter((student) => student.location === city);
+  const updatedStudents = studentsInCity.map((student) => {
+    const newGrade = newGrades.find((grade) => grade.studentId === student.id);
     return {
       ...student,
-      grade: 'N/A',
+      grade: newGrade ? newGrade.grade : 'N/A',
     };
   });
+
+  return updatedStudents;
 }
